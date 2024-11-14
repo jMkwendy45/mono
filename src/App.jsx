c987f85e55b164343953543b060f1a6979262eb8
Hp <duwend46@gmail.com>
2024-11-14 12:52:38 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './component/header/header'
 import InvoiceTemplate from './component/invoice/invoice'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
